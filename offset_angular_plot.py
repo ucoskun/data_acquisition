afdd792a9518e8490d18de56551b77a2a45c7948
Umit Coskun <uhcoskun@outlook.com>
2020-06-25 13:10:53 -0400

    Update and cleanup

--- offset_angular_plot.py
+++ offset_angular_plot.py
@@ -1,16 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-t = [x for x in range(0, 10400, 400)]
-x = np.loadtxt("ang_vs_field2.txt")[:,1]
+data_path = "data_out"
+t = [x * 360 / 20000 for x in range(0, 10400, 400)]
+x = np.loadtxt(data_path + "/ang_vs_field2.txt")[:,1]
 
 off = [(x[i]+x[25+i])/2 for i in range(26)]
+
 for i in range(25):
     print((x[i]+x[25+i])/2)
+
 ax = plt.subplot(111)
 ax.plot(t, off, '.')
 ax.grid(True)
-plt.xlabel('angle (counts)')
-plt.ylabel('field (nT)')
+
+plt.xticks(t)
+plt.xlabel('starting angle (degrees)')
+plt.ylabel('offset (nT)')
 
 plt.show()
